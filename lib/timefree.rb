d29083c05fb8d75dfd3e67f91467394b76394b62
ASAHI,Michiharu <fusuian@gmail.com>
2017-08-07 17:31:11 +0900

    timefreeで録音できない不具合を修正（出力ファイル名の拡張子tmpをaacにした）

--- lib/timefree.rb
+++ lib/timefree.rb
@@ -17,12 +17,17 @@ class TimeFree < RadikoPremium
 		starttime= opts[:from]
 		endtime = starttime + sec/60/60/24
 		filename = opts[:filename]
+		@outdir = opts[:outdir] || '.'
 
 		from = starttime.timefree
 		to   = endtime.timefree
 		# pp [@channel, from, to]
 		@stream_uri=%Q(https://radiko.jp/v2/api/ts/playlist.m3u8?l=15&station_id=#{@channel}&ft=#{from}&to=#{to})
-		radio_play datetime: starttime, filename: filename
+		# radio_play datetime: starttime, filename: filename
+		player = create_player self.class::channels[@channel]
+		dt = datetime starttime
+		recfile = make_recfile filename, dt
+		player.rec recfile, sec
 	end
 
 
