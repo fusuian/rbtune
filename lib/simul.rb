85c89218a95e6d27ea232bac832f251d1af43579
ASAHI,Michiharu <fusuian@gmail.com>
2019-09-23 22:17:51 +0900

    URIを整理

--- lib/simul.rb
+++ lib/simul.rb
@@ -7,14 +7,13 @@ class Simul < Radio
 
 	def self.channels
 		{
-			"tachikawa"   => "mmsh://hdv3.nkansai.tv/FmTachikawa?MSWMExt=.asf",
-			"chofufm"     => "mmsh://hdv3.nkansai.tv/chofu?MSWMExt=.asf",
+			"tachikawa"   => "mmsh://hdv3.nkansai.tv/tachikawa",
+			"chofufm"     => "mmsh://hdv3.nkansai.tv/chofu",
 			"comiten"     => "mms://st1.shimabara.jp/comiten",
-			"midfm"       => "mmsh://203.141.56.46:80/mid-fm761?MSWMExt=.asf",
-			"rainbowtown" => "mmsh://211.1.40.129:80/rainbowtown?MSWMExt=.asf",
-			"tsukuba"     => "mmsh://211.1.40.21:80/tsukuba?MSWMExt=.asf",
+			"midfm"       => "mmsh://simuledge.shibapon.net/mid-fm761",
+			"rainbowtown" => "mmsh://hdv3.nkansai.tv/rainbowtown",
+			"tsukuba"     => "mmsh://hdv4.nkansai.tv/tsukuba",
 			"ishinomaki" => "mms://hdv2.nkansai.tv/ishinomaki"
-			# "marine" => "rtmp://mtist.as.smartstream.ne.jp/30061/livestream/playlist.m3u8"
 		}
 	end
 
