1537cbcf46428244335b23f7390f30a24f4aa52d
ＡＳＡ <fusuian@gmail.com>
2020-05-30 16:19:44 +0900

    NHKR1, NHKR2, NHKFM を東京局に割り当てる

--- lib/rbtune/radiru.rb
+++ lib/rbtune/radiru.rb
@@ -34,21 +34,29 @@ class Radiru < Radio
   def parse_stations(body)
     stations = body.search '//data'
     stationsjp = {
-      'r1'=> "ラジオ第1",
-      'r2'=> "ラジオ第2",
-      'fm'=> "FM",
+      'r1' => "ラジオ第1",
+      'r2' => "ラジオ第2",
+      'fm' => "FM",
     }
-    stations.map do |station|
+    stations = stations.map do |station|
       areajp = station.at('areajp').text
       area   = station.at('area').text
+      # 地区ごとに第1, 第2, FM を登録する
       r1, r2, fm = %w(r1 r2 fm).map do |v|
-        hls = "#{v}hls"
-        id  = "nhk#{v}-#{area}".upcase
+        hls  = "#{v}hls"
+        id   = "nhk#{v}-#{area}".upcase
         uri  = station.at(hls).text
-        name = "NHK#{stationsjp[v]}（#{areajp}）"
+        name = "NHK#{stationsjp[v]}-#{areajp}"
         Station.new(id, uri, name: name, ascii_name: id)
       end
-    end.flatten
+    end
+    stations.flatten!
+    # id: NHKR1, NHKR2, NHKFM を東京局に割り当てる　
+    stations.find_all { |station| station.id =~ /-TOKYO/}.reverse.map do |station|
+      id = station.id.sub(/-TOKYO/, '')
+      stations.unshift Station.new(id, station.uri, name: station.name, ascii_name: id)
+    end
+    stations
   end
 
 end
